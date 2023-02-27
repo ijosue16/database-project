2ce61c7c71ad604f9f243f0b7cbee0185ed12a4a
ijosue16 <iradukundajosue16@gmail.com>
2023-02-27 20:28:38 +0200

    update queries

--- queries.sql
+++ queries.sql
@@ -6,7 +6,7 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 
 -- Query for List the name of all animals born between 2016 and 2019.
 SELECT name FROM animals
-WHERE date_of_birth BETWEEN '2016-1-1'AND '2019-1-1';
+WHERE date_of_birth BETWEEN '2016-1-1'AND '2019-12-31';
 
 -- Query for List the name of all animals that are neutered and have less than 3 escape attempts.
 SELECT name FROM animals
