dd1b943a44219ddcd0d123982cc201750cc8ef35
ijosue16 <iradukundajosue16@gmail.com>
2023-02-28 12:23:33 +0200

    adding queries

--- queries.sql
+++ queries.sql
@@ -32,3 +32,66 @@ WHERE name <> 'Gabumon' ;
 -- Query for Find all animals with a weight between 10.4kg and 17.3kg
 SELECT * FROM animals
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+--Query for transaction to set species ->digimon to animals ending in mon and species->pokemon to animals with no species
+BEGIN;
+UPDATE animals
+SET species ='digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species ='pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+--Query for transaction to delete all data and rollback
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+--Query for transacton to delete animals with dob> 2022 making savepoint and update weight of animal with negative weight
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth >'2022-01-01';
+SAVEPOINT born_before;
+UPDATE animals
+SET weight_kg = weight_kg * (-1);
+ROLLBACK TO born_before;
+UPDATE animals
+SET weight_kg = weight_kg * (-1)
+WHERE weight_kg < -1;
+SELECT * FROM animals;
+COMMIT;
+
+-- Query to find How many animals are there
+SELECT COUNT (id)
+FROM animals;
+
+-- Query to find How many animals have never tried to escape
+SELECT COUNT (id)
+FROM animals
+WHERE escape_attempts < 1;
+
+-- Query to find What is the average weight of animals
+SELECT AVG (weight_kg)
+FROM animals;
+
+-- Query to find Who escapes the most, neutered or not neutered animals
+SELECT name 
+FROM animals
+GROUP BY name
+HAVING  COUNT(escape_attempts )= MAX(escape_attempts);
+
+-- Query to find  is the minimum and maximum weight of each type of animal
+SELECT species,MAX(weight_kg) AS maximum_weight,MIN(weight_kg) AS manimum_weight
+FROM animals
+GROUP BY species
+
+-- Query to find What is the average number of escape attempts per animal type of those born between 1990 and 2000
+SELECT species,AVG(escape_attempts) AS average_escape_attempts_number
+FROM animals
+WHERE date_of_birth BETWEEN'1990-01-01' AND'2000-12-31'
+GROUP BY species
+
+
