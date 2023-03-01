1f91d90f205c60f731221a1c9b26c2ce04608c64
ijosue16 <iradukundajosue16@gmail.com>
2023-03-01 18:39:19 +0200

    inert data and modify tables

--- data.sql
+++ data.sql
@@ -17,3 +17,43 @@ VALUES
 ('Boarmon','2005-06-07',7,true,20.4),
 ('Blossom','1998-08-13',3,true,17),
 ('Ditto','2022-05-14',4,true,22);
+
+-- Insert the following data into the owners table
+INSERT INTO owners(full_name,age)
+VALUES('Sam Smith',34),
+('Jennifer Orwell',19),
+('Bob',45),
+('Melody',77),
+('Dean winchester',14),
+('Jodie Whittaker',38);
+SELECT * FROM owners;
+
+-- Insert the following data into the species table
+
+INSERT INTO species(name)
+VALUES('Pokemon'),
+('Digimon');
+SELECT * FROM species;
+
+
+-- Modify your inserted animals so it includes the species_id value
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
+SELECT * FROM animals;
+
+-- Modify your inserted animals to include owner information (owner_id)
+UPDATE animals
+SET owner_id= 1 WHERE name ='Agumon';
+UPDATE animals
+SET owner_id= 2 WHERE name IN ('Gabumon','Pikachu');
+UPDATE animals
+SET owner_id= 3 WHERE name IN ('Devimon','Plantmon');
+UPDATE animals
+SET owner_id= 4 WHERE name IN ('Charmander','Squirtle','Blossom');
+UPDATE animals
+SET owner_id= 5 WHERE name IN ('Angemon','Boarmon');
+SELECT * FROM animals;
