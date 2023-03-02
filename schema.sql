5b75bc020004194318b9870a9eccf492b58d991f
ijosue16 <iradukundajosue16@gmail.com>
2023-03-02 15:07:38 +0200

    add vets table

--- schema.sql
+++ schema.sql
@@ -44,3 +44,12 @@ ALTER TABLE animals
 ADD CONSTRAINT fk_owners_animals
 FOREIGN KEY(owner_id) REFERENCES owners(id);
 SELECT * FROM animals;
+
+
+-- Create a table named vets with the following columns
+
+CREATE TABLE vets (
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(100),
+age INT,
+date_of_graduation DATE );
