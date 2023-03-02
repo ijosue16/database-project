8ada86763d756281f89f76db69d2374963ade170
ijosue16 <iradukundajosue16@gmail.com>
2023-03-02 15:26:09 +0200

    inert data into vets

--- data.sql
+++ data.sql
@@ -57,3 +57,14 @@ SET owner_id= 4 WHERE name IN ('Charmander','Squirtle','Blossom');
 UPDATE animals
 SET owner_id= 5 WHERE name IN ('Angemon','Boarmon');
 SELECT * FROM animals;
+
+-- Insert the following data for vets
+INSERT INTO vets(name,age,date_of_graduation)
+VALUES(' William Tatcher ',45,'2000-04-23'),
+('Maisy Smith',26,'2019-01-17'),
+('Stephanie Mendez',64,'1981-05-04'),
+('Jack Harkness',38,'2008-06-08');
+
+-- Insert the following data for specialties
+
+-- Insert the following data for visits
